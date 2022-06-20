04433bd2e09689cdf85a621d4692159459491230
code_vik <jitucode.vik@gmail.com>
2022-06-20 12:38:38 +0530

    added some files

---
+++ Keyboard/app.js
@@ -0,0 +1,24 @@
+import { getKeyName,getElementWithKey,makeKeyUnjiggle,makeRandomKeyJiggle,makeKeyRed } from "./utility.js";
+
+const key = document.querySelectorAll(".key");
+
+document.addEventListener("keydown", (event) => { 
+  const keyName = getKeyName(event);
+  const element = getElementWithKey(keyName);
+  
+  if (element.getAttribute("class") === "key jiggle") {
+    makeKeyUnjiggle(element);
+    makeRandomKeyJiggle();
+  } else {
+    makeKeyRed(element);
+  }
+});
+
+document.addEventListener("keyup", (event) => {
+  const keyName = getKeyName(event);
+  const element = getElementWithKey(keyName);
+  if (element.getAttribute("class") === "key red") {
+    element.setAttribute("class", "key");
+  }
+});
+
